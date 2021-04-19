9c394a4086f2a3112f55946c20183cff6640899b
Longjie Chao <chao.long.jie@alumnat.copernic.cat>
2021-04-19 20:01:20 +0200

    cosas

--- src/photoorganizer/PhotoOrganizer.java
+++ src/photoorganizer/PhotoOrganizer.java
@@ -6,62 +6,41 @@
 package photoorganizer;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 
 /**
  *
  * @author LJChao-PC
  */
-public class PhotoOrganizer extends Application{
+public class PhotoOrganizer extends Application {
 
-    public void start(Stage primaryStage) {
-        File f = new File("C:/Users/LJChao-PC/Desktop/Pack 1/img");
-        createGUI(primaryStage, f);
+    public void start(Stage primaryStage) throws IOException {
+        UI ui = new UI("C:/Users/LJChao-PC/Desktop/Pack 1/img", primaryStage);
+        ui.load();
     }
-    
-    public void createGUI(Stage primaryStage, File f){
-        SimpleDateFormat date = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-        BorderPane bPane = new BorderPane();
-        
-        //Top
-        VBox vb = new VBox();
-        TextField txtf = new TextField(f.getPath());
-        vb.getChildren().add(txtf);
-        bPane.setTop(vb);
-        
-        //Center
-        vb = new VBox();
-        for(int i = 0; i < f.listFiles().length; i++){
-            Text txt = new Text("");
-            if(f.listFiles()[i].getName().endsWith(".jpg")){
-                txt = new Text("File: " + f.listFiles()[i].getName() + " Last Time Modified: " + date.format(f.listFiles()[i].lastModified()));
-            }else if(f.listFiles()[i].isDirectory()){
-                txt = new Text("Directory: " + f.listFiles()[i].getName());
-            }
-            vb.getChildren().add(txt);
-        }
-        
-        
-        bPane.setCenter(vb);
-        
-        Scene scene = new Scene(bPane, 800, 600);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-    
-    public static void main(String[] args){
+
+
+
+    public static void main(String[] args) {
         launch(args);
         /*File f1 = new File("C:\\Users\\LJChao-PC\\Desktop\\Pack 1\\img");
         SimpleDateFormat date = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
@@ -76,5 +55,90 @@ public class PhotoOrganizer extends Application{
                 System.out.println("ey");
             }
         }*/
+    }
+}
+
+class UI{
+    private File filePath;
+    private Stage primaryStage;
+    
+    public UI(String filePath, Stage primaryStage){
+        this.filePath = new File(filePath);
+        this.primaryStage = primaryStage;
+    }
+    
+    public void load() throws IOException{
+        createAppUI();
+    }
+
+    public File getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(File filePath) {
+        this.filePath = filePath;
+    }
+    
+    public void createAppUI() throws IOException {
+        BorderPane bPane = new BorderPane();
+        VBox vb;
+        HBox hb;
+        Text txt;
+
+        //Top
+        hb = new HBox();
+        Button button;
+        button = new Button("<");
+        hb.getChildren().add(button);
+        button = new Button(">");
+        hb.getChildren().add(button);
+        button = new Button("^");
+        hb.getChildren().add(button);
+        TextField txtf = new TextField(filePath.getPath());
+        HBox.setHgrow(txtf, Priority.ALWAYS);
+        hb.getChildren().add(txtf);
+        hb.setSpacing(10);
+        bPane.setTop(hb);
+
+        //Center
+        hb = new HBox();
+        for (int i = 0; i < filePath.listFiles().length; i++) {
+            Image image;
+            ImageView imageView = new ImageView();
+            vb = new VBox();
+            if (filePath.listFiles()[i].getName().endsWith(".jpg")) {
+                vb = new VBox();
+                //Imagen
+                image = new Image(new FileInputStream(filePath.listFiles()[i]));
+                imageView = new ImageView(image);
+                imageView.setPreserveRatio(true);
+                imageView.setFitWidth(200);
+                
+                
+                //Nombre Imagen
+                txt = new Text(filePath.listFiles()[i].getName());
+                vb.getChildren().addAll(imageView,txt);
+                vb.setAlignment(Pos.BASELINE_CENTER);
+                
+                //txt = new Text("File: " + f.listFiles()[i].getName() + " Last Time Modified: " + date.format(f.listFiles()[i].lastModified()));
+            }//else if(f.listFiles()[i].isDirectory()){
+            //txt = new Text("Directory: " + f.listFiles()[i].getName());
+            //}
+            //vb.getChildren().add(txt);
+            hb.getChildren().add(vb);
+        }
+        
+        hb.setSpacing(50);
+        bPane.setCenter(hb);
+
+        vb = new VBox();
+        txt = new Text("Tree");
+        vb.getChildren().add(txt);
+        bPane.setLeft(vb);
+
+        //End
+        Scene scene = new Scene(bPane, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
