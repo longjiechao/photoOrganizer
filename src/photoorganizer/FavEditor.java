01c344249b6f1ce438b97c0ac67946fa09a0435a
LJChao3473 <ljchao3473@gmai.com>
2021-04-25 17:50:31 +0200

    add xml

--- src/photoorganizer/FavEditor.java
+++ src/photoorganizer/FavEditor.java
@@ -13,7 +13,16 @@ import org.w3c.dom.Node;
 import org.w3c.dom.Element;  
 import java.io.File;  
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.xml.sax.SAXException;
 
 /**
@@ -26,25 +35,57 @@ public class FavEditor {
     DocumentBuilder db;
     Document doc;
     NodeList nodeList;  
+    Element root;
+    ArrayList<String> allImages;
 
     public FavEditor() throws SAXException, IOException, ParserConfigurationException {
         this.file = new File("./src/photoorganizer/fav.xml");
         dbf = DocumentBuilderFactory.newInstance();
         db = dbf.newDocumentBuilder();  
-        doc = db.parse(file);  
-        doc.getDocumentElement().normalize(); 
+        doc = db.parse(file); 
+        root = doc.getDocumentElement();
+        doc.getDocumentElement().normalize();
         nodeList = doc.getElementsByTagName("imagen");
+        reloadArray();
     }
     
-    public void test(){
+    public void reloadArray(){
+        nodeList = doc.getElementsByTagName("imagen");
+        allImages = new ArrayList<String>();
         for(int i = 0; i < nodeList.getLength(); i++){
             Node node = nodeList.item(i);
             Element e = (Element) node;  
             String path = e.getElementsByTagName("path").item(0).getTextContent().replaceAll("\\s", "");
-            System.out.println("PATH: " + path); 
+            allImages.add(path);
+            System.out.println(path);
         }
-        
-        
     }
     
+    public ArrayList<String> getAllImages(){
+        return allImages;
+    }
+    
+    public void addElement(String newImage) throws TransformerException{
+        Element newImagen = doc.createElement("imagen");
+        Element newPath = doc.createElement("path");
+        newPath.setTextContent(newImage);
+        newImagen.appendChild(newPath);
+        root.appendChild(newImagen);
+        saveFile();
+        reloadArray();
+    }
+    public void deleteElement(){
+        
+        reloadArray();
+    }
+    public boolean contains(String check){
+        return allImages.contains(check);
+    }
+    
+    public void saveFile() throws TransformerConfigurationException, TransformerException{
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        Result output = new StreamResult(file); // xml is a object of File i.e. File xml = new File(filePath);
+        Source input = new DOMSource(doc);
+        transformer.transform(input, output);
+    }
 }
--- src/photoorganizer/test.java
+++ src/photoorganizer/test.java
@@ -2,13 +2,15 @@ package photoorganizer;
 
 import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import org.xml.sax.SAXException;
 
 
 public class test{
-    public static void main (String args[]) throws SAXException, IOException, ParserConfigurationException{
+    public static void main (String args[]) throws SAXException, IOException, ParserConfigurationException, TransformerException{
         FavEditor fe = new FavEditor();
-        fe.test();
+        fe.addElement("NEW TEST");
+        System.out.println(fe.getAllImages().toString());
         
     }
 }
